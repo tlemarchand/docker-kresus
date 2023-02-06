15b162b3bc398f3cce658820a23bbd1c263526f9
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-02-06 20:05:10 +0100

    Version update

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ curl -s 'https://pypi.org/pypi/woob/json' | jq -j -r '.info.version' > woob_vers
 curl -s 'curl -s 'curl -s 'https://registry.npmjs.org/kresus/latest' | jq -j -r '.version' > kresus_version
 
 git add -A
-git commit -m `cat kresus_version`-`cat woob_version`
+git commit -m "Version update"
 git push origin main
 git tag `cat kresus_version`-`cat woob_version`
 git push origin `cat kresus_version`-`cat woob_version`
